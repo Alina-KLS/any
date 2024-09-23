aefa3cdb87e1560999962a566e90a18819ef7125
Alina-KLS <alina22love02family@mail.ru>
2024-09-23 16:54:43 +0300

    Add files via upload

---
+++ lecture_01.py
@@ -0,0 +1,14 @@
+from math import sqrt
+from math import cos
+from math import radians
+a = int(input("Введите длину первой стороны: "))
+b = int(input("Введите длину второй стороны: "))
+x = int(input("Введите величину угла в градусах: "))
+x1 = radians(x)
+
+c = sqrt(a**2+b**2-2*a*b*cos(x1))
+
+if a+b>c and a+c>b and b+c>a and 0<=x<=360:
+    print("Третья сторона: ", c)
+else:
+    print("Такого треугольника не существует.")
